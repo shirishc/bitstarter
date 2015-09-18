4d20c2e6fa5030f6918a4879aedb3c2a9f893478
Shirish Chandrakar <shirishc@hotmail.com>
2015-09-18 18:50:15 +0900

    index.js reading index.html

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
+    var fs = require('fs');
     var buf = fs.readFileSync('./index.html');
     response.send(buf.toString());
 })
